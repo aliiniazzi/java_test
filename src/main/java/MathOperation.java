06ce92df0617fd1fca10d3ae22dc2d356a55333c
Ali Niazi <aliiniazzi@gmail.com>
2025-11-24 15:14:11 +0330

    Test : add basic math operation tests with GWT and assertions

---
+++ src/main/java/MathOperation.java
@@ -0,0 +1,19 @@
+public class MathOperation {
+
+     public double add (double numberOne , double numberTwo){
+         return numberOne + numberTwo;
+     }
+
+     public double subtraction(double numberOne , double numberTwo){
+         return numberOne - numberTwo;
+     }
+
+     public double multiply(double numberOne , double numberTwo){
+         return numberOne * numberTwo;
+     }
+
+     public double divide(double numberOne , double numberTwo){
+         return  numberOne / numberTwo;
+     }
+
+}
--- src/test/java/MathOperationTest.java
+++ src/test/java/MathOperationTest.java
@@ -1,7 +1,19 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class MathOperationTest {
+
+    MathOperation mathOperation;
+
+    @BeforeAll
+    void setup(){
+        mathOperation = new MathOperation();
+    }
+
+    // without instance
     @Test
     void ShouldThrowZeroException(){
         // Given
@@ -16,4 +28,31 @@ public class MathOperationTest {
             int result = numberOne / numberTwo;
         });
     }
+
+    // with instance
+    @Test
+    void shouldCalculateBasicOperationsCorrectly(){
+
+        // Given
+        double numberOne = 10;
+        double numberTwo = 5;
+
+        // When
+        double addResult = mathOperation.add(numberOne , numberTwo);
+        double subtractionResult = mathOperation.subtraction(numberOne , numberTwo);
+        double multiplyResult = mathOperation.multiply(numberOne , numberTwo);
+        double divideResult = mathOperation.divide(numberOne , numberTwo);
+
+
+        // Then
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(15 , addResult),
+                () -> Assertions.assertEquals(5 , subtractionResult),
+                () -> Assertions.assertEquals(50 , multiplyResult),
+                () -> Assertions.assertEquals(2 , divideResult)
+        );
+    }
+
+
+
 }
